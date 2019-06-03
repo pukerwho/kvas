52c2377058aca52bea6af5c1b7bc5d7dc2cdfbf0
Vladimir Samojlov <v-moi@ya.ru>
2019-06-03 10:46:49 +0300

    minor fix

--- tpl_main.php
+++ tpl_main.php
@@ -123,7 +123,7 @@ Template Name: Главная страница
 		<div class="p_main__brand-content">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-md-4">
 						<div class="animate-puk-mask">
 							<div class="p_main__brand-title animate-puk" data-effect="fade-up" data-delay="0.8s">
 								<?php
