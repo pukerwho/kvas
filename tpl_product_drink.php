cf4c753a1ceec4dd8b970fbd6146bd6e888860f1
Vladimir Samojlov <v-moi@ya.ru>
2019-06-18 16:01:38 +0300

    edit mobile fix

--- tpl_product_drink.php
+++ tpl_product_drink.php
@@ -21,36 +21,84 @@ Template Name: Продукты НАПИТКИ
 					</h1>
 				</div>
 			</div>
-			<div class="row">
-				<?php
-				$custom_query_products = new WP_Query( array( 
-					'post_type' => 'products',
-					'meta_query' => array(
-						array(
-							'key'     => 'crb_product_drink',
-							'value'   => 'no',
-							'compare' => '=',
-						),
-					)
-				) );
-				if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
-				<div class="col-6 col-md-4 col-lg-4">
-					<div class="p_products__slide" data-product-slide="product-<?php echo get_the_id(); ?>">
-						<div class="p_products__slide-top">
-							<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
-							<div class="p_products__slide-photo">
-								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+		</div>
+		<div class="pc-show">
+			<div class="container">
+				<div class="row">
+					<?php
+					$custom_query_products = new WP_Query( array( 
+						'post_type' => 'products',
+						'meta_query' => array(
+							array(
+								'key'     => 'crb_product_drink',
+								'value'   => 'no',
+								'compare' => '=',
+							),
+						)
+					) );
+					if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
+					<div class="col-6 col-md-4 col-lg-4">
+						<div class="p_products__slide" data-product-slide="product-<?php echo get_the_id(); ?>">
+							<div class="p_products__slide-top">
+								<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
+								<div class="p_products__slide-photo">
+									<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+								</div>
+								<div class="p_products__slide-volume">
+									<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+								</div>
 							</div>
-							<div class="p_products__slide-volume">
-								<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+							<div class="p_products__slide-title">
+								<?php the_title(); ?>	
 							</div>
 						</div>
-						<div class="p_products__slide-title">
-							<?php the_title(); ?>	
+					</div>
+					<?php endwhile; endif; ?>
+				</div>
+			</div>
+		</div>
+		<div class="mobile-show">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="swiper-products swiper-container">
+							<div class="swiper-wrapper">
+								<?php
+								$custom_query_products = new WP_Query( array( 
+									'post_type' => 'products',
+									'meta_query' => array(
+										array(
+											'key'     => 'crb_product_drink',
+											'value'   => 'no',
+											'compare' => '=',
+										),
+									)
+								) );
+								if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
+									<div class="p_products__slide swiper-slide" data-product-slide="product-<?php echo get_the_id(); ?>">
+										<div class="p_products__slide-top">
+											<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
+											<div class="p_products__slide-photo">
+												<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+											</div>
+											<div class="p_products__slide-volume">
+												<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+											</div>
+										</div>
+										<div class="p_products__slide-title">
+											<?php the_title(); ?>	
+										</div>
+									</div>
+								<?php endwhile; endif; ?>
+							</div>
+							<div class="swiper-button-next swiper-products-button-next"></div>
+							<div class="swiper-button-prev swiper-products-button-prev"></div>
+						</div>
+						<div class="d-flex justify-content-center">
+							<div class="swiper-mainproducts-pagination"></div>	
 						</div>
 					</div>
 				</div>
-				<?php endwhile; endif; ?>
 			</div>
 		</div>
 	</div>
--- tpl_products.php
+++ tpl_products.php
@@ -11,60 +11,97 @@ Template Name: Страница ПРОДУКТЫ
 <div class="p_products">
 	<div class="p_products__block">
 		<?php get_template_part('blocks/b_products') ?>	
-		<div class="container">
-			<div class="row">
-				<?php
-				$custom_query_products = new WP_Query( array( 'post_type' => 'products') );
-				if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
-				<div class="col-6 col-md-4 col-lg-4">
-					<div class="p_products__slide" data-product-slide="product-<?php echo get_the_id(); ?>">
-						<div class="p_products__slide-top">
-							<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
-							<div class="p_products__slide-photo">
-								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-							</div>
-							<div class="p_products__slide-volume">
-								<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
-							</div>
-						</div>
-						<div class="p_products__slide-title">
-							<?php the_title(); ?>	
-						</div>
-					</div>
-				</div>
-				<?php endwhile; endif; ?>
-			</div>
-			<!-- <div class="row">
-				<div class="col-md-12">
-					<div class="swiper-products swiper-container">
-						<div class="swiper-wrapper">
-							<?php
-							$custom_query_products = new WP_Query( array( 'post_type' => 'products') );
-							if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
-								<div class="p_products__slide swiper-slide" data-product-slide="product-<?php echo get_the_id(); ?>">
-									<div class="p_products__slide-top">
-										<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
-										<div class="p_products__slide-photo">
-											<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-										</div>
-										<div class="p_products__slide-volume">
-											<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
-										</div>
-									</div>
-									<div class="p_products__slide-title">
-										<?php the_title(); ?>	
-									</div>
+		<div class="pc-show">
+			<div class="container">
+				<div class="row">
+					<?php
+					$custom_query_products = new WP_Query( array( 'post_type' => 'products') );
+					if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
+					<div class="col-md-4 col-lg-4">
+						<div class="p_products__slide" data-product-slide="product-<?php echo get_the_id(); ?>">
+							<div class="p_products__slide-top">
+								<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
+								<div class="p_products__slide-photo">
+									<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
 								</div>
-							<?php endwhile; endif; ?>
+								<div class="p_products__slide-volume">
+									<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+								</div>
+							</div>
+							<div class="p_products__slide-title">
+								<?php the_title(); ?>	
+							</div>
 						</div>
-						<div class="swiper-button-next swiper-products-button-next"></div>
-						<div class="swiper-button-prev swiper-products-button-prev"></div>
 					</div>
-					<div class="d-flex justify-content-center">
-						<div class="swiper-pagination"></div>	
+					<?php endwhile; endif; ?>
+				</div>
+				<!-- <div class="row">
+					<div class="col-md-12">
+						<div class="swiper-products swiper-container">
+							<div class="swiper-wrapper">
+								<?php
+								$custom_query_products = new WP_Query( array( 'post_type' => 'products') );
+								if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
+									<div class="p_products__slide swiper-slide" data-product-slide="product-<?php echo get_the_id(); ?>">
+										<div class="p_products__slide-top">
+											<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
+											<div class="p_products__slide-photo">
+												<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+											</div>
+											<div class="p_products__slide-volume">
+												<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+											</div>
+										</div>
+										<div class="p_products__slide-title">
+											<?php the_title(); ?>	
+										</div>
+									</div>
+								<?php endwhile; endif; ?>
+							</div>
+							<div class="swiper-button-next swiper-products-button-next"></div>
+							<div class="swiper-button-prev swiper-products-button-prev"></div>
+						</div>
+						<div class="d-flex justify-content-center">
+							<div class="swiper-pagination"></div>	
+						</div>
+					</div>
+				</div> -->
+			</div>
+		</div>
+		<div class="mobile-show">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="swiper-products swiper-container">
+							<div class="swiper-wrapper">
+								<?php
+								$custom_query_products = new WP_Query( array( 'post_type' => 'products') );
+								if ($custom_query_products->have_posts()) : while ($custom_query_products->have_posts()) : $custom_query_products->the_post(); ?>
+									<div class="p_products__slide swiper-slide" data-product-slide="product-<?php echo get_the_id(); ?>">
+										<div class="p_products__slide-top">
+											<div class="p_products__slide-cover" style="background-color: <?php echo carbon_get_the_post_meta('crb_product_color') ?>;"></div>
+											<div class="p_products__slide-photo">
+												<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+											</div>
+											<div class="p_products__slide-volume">
+												<img src="<?php bloginfo('template_url') ?>/img/volume.svg" alt=""> <?php echo carbon_get_the_post_meta('crb_product_volume') ?>
+											</div>
+										</div>
+										<div class="p_products__slide-title">
+											<?php the_title(); ?>	
+										</div>
+									</div>
+								<?php endwhile; endif; ?>
+							</div>
+							<div class="swiper-button-next swiper-products-button-next"></div>
+							<div class="swiper-button-prev swiper-products-button-prev"></div>
+						</div>
+						<div class="d-flex justify-content-center">
+							<div class="swiper-mainproducts-pagination"></div>	
+						</div>
 					</div>
 				</div>
-			</div> -->
+			</div>
 		</div>
 	</div>
 	<?php
